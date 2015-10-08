b9a7b44407ed5af3b6a962fe9f3ea531bc215340
Jack Stevens <jack@applyr.com>
2015-10-09 00:53:14 +0100

    Add Ampersand support

---
+++ packages/ampersand/scenarios.js
@@ -0,0 +1,26 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      dependencies: {
+        'ampersand': '1.x',
+        'not-ampersand': '2.x'
+      },
+      devDependencies: {
+        'ampersand-collection': '3.x',
+        'not-ampersand': '4.x'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    version: '1.x',
+    packageName: 'ampersand',
+    dependencyGroup: 'dependencies'
+  }, {
+    src: 'packageJson',
+    version: '3.x',
+    packageName: 'ampersand-collection',
+    dependencyGroup: 'devDependencies'
+  }]
+}];
