e92fa8d0c7b8e1900f7b244d89a916a2fa7b9f28
Jack Stevens <jack@applyr.com>
2015-11-09 23:19:05 +0000

    Add Component package support (even though it has been deprecated...)

---
+++ packages/component/scenarios.js
@@ -0,0 +1,30 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        component: '1.*',
+        'not-component': '2.*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    dependencyGroup: 'devDependencies',
+    packageName: 'component',
+    version: '1.*'
+  }]
+}, {
+  description: 'when found in file paths',
+  input: {
+    filePaths: [
+      'component.json',
+      'not-in-root/component.json',
+      'component.JSON'
+    ]
+  },
+  output: [{
+    src: 'file',
+    filePath: 'component.json'
+  }]
+}];
