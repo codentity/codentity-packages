63d73fd9b74fce77b8842d6acbd3a8fcd3fed75f
Jack Stevens <jack@applyr.com>
2015-11-09 21:43:26 +0000

    Add jspm package support (minus config.js)

---
+++ packages/jspm/scenarios.js
@@ -0,0 +1,18 @@
+// NOTE we do not attempt to cover `config.js` as the file naming convention is too general.
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        jspm: '1.*',
+        'not-jspm': '2.*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    dependencyGroup: 'devDependencies',
+    packageName: 'jspm',
+    version: '1.*'
+  }]
+}];
