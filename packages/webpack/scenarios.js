dcb1e360f4fa984371cc95716e4ef3ea557da319
Jack Stevens <jack@applyr.com>
2015-11-09 21:44:14 +0000

    Add Webpack package support

---
+++ packages/webpack/scenarios.js
@@ -0,0 +1,41 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        webpack: '1.*',
+        'gulp-webpack': '2.*',
+        'grunt-webpack': '3.*',
+        'not-webpack': '4.*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    dependencyGroup: 'devDependencies',
+    packageName: 'webpack',
+    version: '1.*'
+  }, {
+    src: 'packageJson',
+    dependencyGroup: 'devDependencies',
+    packageName: 'gulp-webpack',
+    version: '2.*'
+  }, {
+    src: 'packageJson',
+    dependencyGroup: 'devDependencies',
+    packageName: 'grunt-webpack',
+    version: '3.*'
+  }]
+}, {
+  description: 'when found in file paths',
+  input: {
+    filePaths: [
+      'webpack.config.js',
+      'nested/webpack.config.js'
+    ]
+  },
+  output: [{
+    src: 'file',
+    filePath: 'webpack.config.js'
+  }]
+}];
