578d848593cd19670dcd9fa28843d8d4e49aa310
Jack Stevens <jack@applyr.com>
2015-11-11 21:57:41 +0000

    Add Jake package support

---
+++ packages/jake/scenarios.js
@@ -0,0 +1,31 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        'jake': '*',
+        'not-jake': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    version: '*',
+    packageName: 'jake',
+    dependencyGroup: 'devDependencies'
+  }]
+}, {
+  description: 'when found in file paths',
+  input: {
+    filePaths: [
+      'config/Jakefile',
+      'Jakefile',
+      'JAKEFILE',
+      'Jakefile.json'
+    ]
+  },
+  output: [{
+    src: 'file',
+    filePath: 'Jakefile'
+  }]
+}];
