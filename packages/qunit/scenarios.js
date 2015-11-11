fcf74632d463b0f9139e0e73e9e9126ac38dd6a9
Jack Stevens <jack@applyr.com>
2015-11-11 20:41:12 +0000

    Add QUnit package support

---
+++ packages/qunit/scenarios.js
@@ -0,0 +1,61 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        'qunit': '*',
+        'qunitjs': '*',
+        'not-qunitjs': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    version: '*',
+    packageName: 'qunitjs',
+    dependencyGroup: 'devDependencies'
+  }]
+}, {
+  description: 'when found in bower.json',
+  input: {
+    bowerJson: {
+      devDependencies: {
+        'qunit': '*',
+        'qunitjs': '*',
+        'not-qunitjs': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'bowerJson',
+    version: '*',
+    packageName: 'qunit',
+    dependencyGroup: 'devDependencies'
+  }]
+}, {
+  description: 'when found in file paths',
+  input: {
+    filePaths: [
+      'vendor/lib/qunit-1.20.0.js',
+      'vendor/lib/qunit-1.20.0.css',
+      'vendor/lib/qunit.js',
+      'vendor/lib/qunit.css',
+      'qunit-1.20.0.js',
+      'qunit-1.20.0.css',
+      'qunit.js'
+    ]
+  },
+  output: [{
+    src: 'file',
+    filePath: 'vendor/lib/qunit-1.20.0.js'
+  }, {
+    src: 'file',
+    filePath: 'vendor/lib/qunit-1.20.0.css'
+  }, {
+    src: 'file',
+    filePath: 'qunit-1.20.0.js'
+  }, {
+    src: 'file',
+    filePath: 'qunit-1.20.0.css'
+  }]
+}];
