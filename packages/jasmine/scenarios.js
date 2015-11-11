7cc05600f6c16c9fdbc80ba645f4b9d18f9aeece
Jack Stevens <jack@applyr.com>
2015-11-11 20:44:24 +0000

    Add Jasmine package support

---
+++ packages/jasmine/scenarios.js
@@ -0,0 +1,47 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        'jasmine-node': '*',
+        'jasmine': '*',
+        'karma-jasmine': '*',
+        'not-jasmine': '*',
+        'jasmine-fake': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    version: '*',
+    packageName: 'jasmine',
+    dependencyGroup: 'devDependencies'
+  }, {
+    src: 'packageJson',
+    version: '*',
+    packageName: 'jasmine-node',
+    dependencyGroup: 'devDependencies'
+  }, {
+    src: 'packageJson',
+    version: '*',
+    packageName: 'karma-jasmine',
+    dependencyGroup: 'devDependencies'
+  }]
+}, {
+  description: 'when found in bower.json',
+  input: {
+    bowerJson: {
+      devDependencies: {
+        'jasmine': '*',
+        'not-jasmine': '*',
+        'jasmine-fake': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'bowerJson',
+    version: '*',
+    packageName: 'jasmine',
+    dependencyGroup: 'devDependencies'
+  }]
+}];
