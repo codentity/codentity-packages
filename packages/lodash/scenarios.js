c2e222214b4ad725000b026514f9c78584701421
Jack Stevens <jack@applyr.com>
2015-11-12 01:13:05 +0000

    Add Lodash package support

---
+++ packages/lodash/scenarios.js
@@ -0,0 +1,51 @@
+module.exports = [{
+  description: 'when found in bower.json',
+  input: {
+    bowerJson: {
+      dependencies: {
+        'lodash': '*',
+        'not-lodash': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'bowerJson',
+    version: '*',
+    packageName: 'lodash',
+    dependencyGroup: 'dependencies'
+  }]
+}, {
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      dependencies: {
+        'lodash': '*',
+        'not-lodash': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    version: '*',
+    packageName: 'lodash',
+    dependencyGroup: 'dependencies'
+  }]
+}, {
+  description: 'when found in file paths',
+  input: {
+    filePaths: [
+      'vendor/lib/lodash.js',
+      'vendor/lib/lodash.min.js',
+      'lodash.min.js',
+      'lodash.js',
+      'not-lodash.js',
+    ]
+  },
+  output: [{
+    src: 'file',
+    filePath: 'vendor/lib/lodash.js'
+  }, {
+    src: 'file',
+    filePath: 'lodash.js'
+  }]
+}];
