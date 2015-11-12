0937b972b17140e8e355da80d2b3cf4edafab342
Jack Stevens <jack@applyr.com>
2015-11-12 00:25:09 +0000

    Add Yo package support

---
+++ packages/yo/scenarios.js
@@ -0,0 +1,34 @@
+module.exports = [{
+  description: 'when found in package.json',
+  input: {
+    packageJson: {
+      devDependencies: {
+        'yo': '*',
+        'yeoman': '*',
+        'not-yo': '*'
+      }
+    }
+  },
+  output: [{
+    src: 'packageJson',
+    version: '*',
+    packageName: 'yo',
+    dependencyGroup: 'devDependencies'
+  }]
+}, {
+  description: 'when found in file paths',
+  input: {
+    filePaths: [
+      'config/.yo-rc.json',
+      'config/yo-rc.json',
+      'config/.yo-rc',
+      '.yo-rc.json',
+      'yo-rc.json',
+      '.yo-rc',
+    ]
+  },
+  output: [{
+    src: 'file',
+    filePath: '.yo-rc.json'
+  }]
+}];
