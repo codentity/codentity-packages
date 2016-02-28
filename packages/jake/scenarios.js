0691e9dfc2116ba32d0fc4338d22aa65403f522f
Jack Stevens <jpstevens@users.noreply.github.com>
2016-02-28 17:52:09 +0000

    Update jake

--- packages/jake/scenarios.js
+++ packages/jake/scenarios.js
@@ -9,6 +9,7 @@ module.exports = [{
     }
   },
   output: [{
+    plugin: 'npm',
     src: 'packageJson',
     version: '*',
     packageName: 'jake',
@@ -25,6 +26,7 @@ module.exports = [{
     ]
   },
   output: [{
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'Jakefile'
   }]
