374729e3f3655ad0eb930af7c408b45a58066880
Jack Stevens <jpstevens@users.noreply.github.com>
2016-02-28 18:04:10 +0000

    Update canjs

--- packages/canjs/scenarios.js
+++ packages/canjs/scenarios.js
@@ -9,6 +9,7 @@ module.exports = [{
     }
   },
   output: [{
+    plugin: 'bower',
     src: 'bowerJson',
     version: '*',
     packageName: 'canjs',
@@ -25,6 +26,7 @@ module.exports = [{
     }
   },
   output: [{
+    plugin: 'npm',
     src: 'packageJson',
     version: '*',
     packageName: 'canjs',
@@ -34,51 +36,62 @@ module.exports = [{
   description: 'when found in file paths',
   input: {
     filePaths: [
-      "can.dojo.js",
-      "can.dojo.min.js",
-      "can.jquery.js",
-      "can.jquery.min.js",
-      "can.mootools.js",
-      "can.mootools.min.js",
-      "can.yui.js",
-      "can.yui.min.js",
-      "can.zepto.js",
-      "can.zepto.min.js",
-      "vendor/lib/can.zepto.min.js",
-      "can.not-really-canjs.min.js"
+      'can.dojo.js',
+      'can.dojo.min.js',
+      'can.jquery.js',
+      'can.jquery.min.js',
+      'can.mootools.js',
+      'can.mootools.min.js',
+      'can.yui.js',
+      'can.yui.min.js',
+      'can.zepto.js',
+      'can.zepto.min.js',
+      'vendor/lib/can.zepto.min.js',
+      'can.not-really-canjs.min.js'
     ]
   },
   output: [{
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.dojo.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.dojo.min.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.jquery.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.jquery.min.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.mootools.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.mootools.min.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.yui.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.yui.min.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.zepto.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'can.zepto.min.js'
   }, {
+    plugin: 'file',
     src: 'filePaths',
     filePath: 'vendor/lib/can.zepto.min.js'
   }]
