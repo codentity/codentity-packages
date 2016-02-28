bd700436740c6d444ffcca2161448ac207cc8681
Jack Stevens <jpstevens@users.noreply.github.com>
2016-02-28 17:42:35 +0000

    Update yui

--- packages/yui/scenarios.js
+++ packages/yui/scenarios.js
@@ -9,6 +9,7 @@ module.exports = [{
     }
   },
   output: [{
+    plugin: 'bower',
     src: 'bowerJson',
     version: '*',
     packageName: 'yui',
@@ -25,6 +26,7 @@ module.exports = [{
     }
   },
   output: [{
+    plugin: 'npm',
     src: 'packageJson',
     version: '*',
     packageName: 'yui',
